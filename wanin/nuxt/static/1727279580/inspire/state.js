aaf833c66d56a61c9c0fa7118baa5cf5be3a0d10
FayW705 <38228674+FayW705@users.noreply.github.com>
2024-09-27 14:00:58 +0800

    Delete wanin directory

--- wanin/nuxt/static/1727279580/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:".\u002Fwanin\u002Fnuxt\u002Fstatic\u002F1727288777",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002F",assetsPath:".\u002Fwanin\u002Fnuxt\u002F",cdnURL:a}}}}(null));
